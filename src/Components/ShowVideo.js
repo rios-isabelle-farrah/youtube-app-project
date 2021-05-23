6ad56ac788234cf4030927afaf50176512b74273
vrwatson <vanessawatson@pursuit.org>
2021-05-23 16:25:15 -0400

    comment merge

--- src/Components/ShowVideo.js
+++ src/Components/ShowVideo.js
@@ -5,13 +5,35 @@ import { useHistory, useParams } from 'react-router-dom'
 
 import "./ShowVideo.css";
 
-const ShowVideo = () => {
+const ShowVideo = ({
+  fullName, 
+  setFullName,
+  comment,
+  setComment,
+  list,
+  setList
+}) => {
   const { videoID } = useParams();
   const history = useHistory();
 
   const goBack = () => {
     history.goBack();
   }
+
+  const handleName = (e) => {
+    setFullName(e.target.value);
+  };
+
+  const handleComment = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList((prevList) => [...prevList, fullName + ": " + comment]);
+    setFullName("");
+    setComment("");
+  };
 
 
   return (
@@ -25,6 +47,32 @@ const ShowVideo = () => {
         height="240px"
         url={`https://www.youtube.com/watch?v=${videoID}`}
       />
+<br></br>
+<br></br>
+<form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="fullName"
+          placeholder="your name"
+          value={fullName}
+          onChange={handleName}
+        />
+        <input
+          type="text"
+          name="comment"
+          placeholder="your comment"
+          value={comment}
+          onChange={handleComment}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <ul>
+        {list.map((oneComment) => {
+          return <li key={oneComment}>{oneComment}</li>;
+        })}
+      </ul>
+
+
     </div>
   );
 };
